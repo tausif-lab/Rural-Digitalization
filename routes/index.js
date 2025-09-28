fd3e2f8ac3d4fb8fb0182c40c19d3423ae1a90f7
tausif-lab <tautumhare@gmail.com>
2025-09-29 02:51:08 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const viewRoutes = require('./viewRoutes');
 const authRoutes = require('./authRoutes');
 const examRoutes = require('./examRoutes');
-const questionRoutes = require('./questionRoutes');
+/*const questionRoutes = require('./questionRoutes');*/
 const lectureRoutes = require('./lectureRoutes');
 //const teacherRoutes = require('./teacherRoutes');
 
@@ -13,7 +13,7 @@ const router = express.Router();
 router.use ('/api/auth',authRoutes);
 /*router.use('/api/teacher', teacherRoutes);*/  
 router.use('/api/exams', examRoutes);      // Exam management routes
-router.use('/api/questions', questionRoutes); // Question management routes
+/*router.use('/api/questions', questionRoutes); // Question management routes*/
 router.use('/api/lectures', lectureRoutes); // Lecture management routes
 router.use('/', viewRoutes); 
             // View routes (HTML pages)
